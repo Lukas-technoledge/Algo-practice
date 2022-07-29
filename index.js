765ee60b1b2adaa195ce9bf1eccb52bb97011987
Onuoha chialuka prisca-mary <83506386+Lukas-technoledge@users.noreply.github.com>
2022-07-29 20:06:19 +0000

    ...

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function prime(n) {
-  if (n < 2) {
+  if (n < 1) {
     return false
   }
-
-  for (let i = 2; i < n; i++) {
-    if (n%i === 0) {
+  while(n > 1) {
+    if (n%2 !== 0) {
       return false
     }
+    n = n%2
   }
-  return true;
+  return true
 }
  
 console.log(prime(5));
