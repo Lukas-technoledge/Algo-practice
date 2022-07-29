0491d63a9a34d1466722af193f0d438b8a85d7a5
Onuoha chialuka prisca-mary <83506386+Lukas-technoledge@users.noreply.github.com>
2022-07-29 20:13:16 +0000

    Bitwise operation using algorithms

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-function prime(n) {
+function powerOfTwo(n) {
   if (n < 1) {
     return false
   }
-  while(n > 1) {
-    if (n%2 !== 0) {
-      return false
-    }
-    n = n%2
-  }
-  return true
+  return ((n & (n-1)) === 0)
 }
  
-console.log(prime(5));
+console.log(powerOfTwo(2));
